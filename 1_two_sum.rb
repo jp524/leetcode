853652f5ab898ee42250c65a1a398a0134642b0b
JP <85654561+jp524@users.noreply.github.com>
2024-04-12 13:20:13 -0400

    1 Two Sum - better solution

--- 1_two_sum.rb
+++ 1_two_sum.rb
@@ -4,19 +4,19 @@
 def two_sum(nums, target)
   return [0, 1] if nums.count == 2
 
+  nums_hash = {}
   nums.each_with_index do |n, i|
     m_target = target - n
-    nums.each_with_index do |m, j|
-      next if j <= i
-      return [i, j] if m == m_target
+
+    if j = nums_hash[m_target]
+      return [i, j]
+    else
+      nums_hash[n] = i
     end
   end
-
 end
 
 p two_sum([2, 7, 11, 15], 9) # [0, 1]
 p two_sum([3, 2, 4], 6) # [1, 2]
 p two_sum([3, 3], 6) # [0, 1]
 p two_sum([3, 2, 3], 6) # [0, 2]
-
-# Valid but times out on LeetCode
