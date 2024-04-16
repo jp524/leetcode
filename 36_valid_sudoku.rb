bcc2463814a2a2ca5b85b5f24c3abd668f60b07d
JP <85654561+jp524@users.noreply.github.com>
2024-04-16 07:08:12 -0400

    36 Valid Sudoku - Faster

--- 36_valid_sudoku.rb
+++ 36_valid_sudoku.rb
@@ -3,42 +3,21 @@
 # @param {Character[][]} board
 # @return {Boolean}
 def is_valid_sudoku(board)
-  rows = board
-  columns = board.transpose
-  sub_boxes = parse_sub_boxes(board)
-
-  valid_arrays?(rows) && valid_arrays?(columns) && valid_arrays?(sub_boxes)
-end
-
-def valid_arrays?(arrays)
-  arrays.each do |array|
-    return false if !valid_array?(array)
-  end
-
-  return true
-end
-
-def parse_sub_boxes(board)
+  columns = Hash.new { |h, k| h[k] = [] }
+  rows = Hash.new { |h, k| h[k] = [] }
   sub_boxes = Hash.new { |h, k| h[k] = [] }
+
   board.each_with_index do |row, i|
     row.each_with_index do |cell, j|
+      next if cell == '.'
+
+      return false if rows[i].include?(cell) || columns[j].include?(cell) || sub_boxes[[i / 3, j / 3]].include?(cell)
+
+      columns[j] << cell
+      rows[i] << cell
       sub_boxes[[i / 3, j / 3]] << cell
     end
   end
-  sub_boxes.values
-end
-
-def valid_array?(array)
-  array.delete('.')
-  ascii_array = []
-  array.each do |num|
-    ascii = num.ord
-    return false unless ascii.between?(49, 57)
-
-    ascii_array << ascii
-  end
-
-  return false if ascii_array.uniq.length != ascii_array.length
 
   true
 end
@@ -53,7 +32,7 @@ board =
    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
    ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
-# p is_valid_sudoku(board) # true
+p is_valid_sudoku(board) # true
 
 board =
   [['8', '3', '.', '.', '7', '.', '.', '.', '.'],
@@ -65,7 +44,7 @@ board =
    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
    ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
-# p is_valid_sudoku(board) # false
+p is_valid_sudoku(board) # false
 
 board =
   [['.', '.', '4', '.', '.', '.', '6', '3', '.'],
