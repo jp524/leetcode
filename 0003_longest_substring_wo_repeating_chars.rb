45ddf665044905e84292b5bdc4be82dda4263d9d
JP <85654561+jp524@users.noreply.github.com>
2024-04-29 12:51:14 -0500

    3 Longest Substring Without Repeating Characters: using Set

--- 0003_longest_substring_wo_repeating_chars.rb
+++ 0003_longest_substring_wo_repeating_chars.rb
@@ -2,20 +2,18 @@
 # @return {Integer}
 def length_of_longest_substring(s)
   l = 0
-  r = 0
-  window = Hash.new(0)
+  window = Set.new
   longest = 0
   
-  while l <= r && r < s.length
-    r_char = s[r]
-
-    if window[r_char] != 0
-      window[s[l]] -=1
-      l -= 1
+  (0...s.length).each do |r|
+    while window.include?(s[r])
+      # Reduce the window
+      window.delete(s[l]) 
+      l += 1
     end
-    window[r_char] += 1
-    r += 1
-    longest = [longest, window.values.count].max
+
+    window.add(s[r])
+    longest = [longest, window.size].max
   end
   
   longest
@@ -23,7 +21,7 @@ end
 
 p length_of_longest_substring("abcabcbb") # 3 "abc"
 p length_of_longest_substring("bbbbb") # 1 "b"
-p length_of_longest_substring("pwwkew") # 3 "wke" # this returns 4
+p length_of_longest_substring("pwwkew") # 3 "wke"
 p length_of_longest_substring("aab") # 2 "ab"
 p length_of_longest_substring("dvdf") # 3 "vdf"
 p length_of_longest_substring("anviaj") # 5 "nviaj"
