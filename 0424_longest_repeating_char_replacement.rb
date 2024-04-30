7c964206a6e7e250155e9ce937d43c184779d264
JP <85654561+jp524@users.noreply.github.com>
2024-04-30 17:48:18 -0500

    424. Longest Repeating Character Replacement

---
+++ 0424_longest_repeating_char_replacement.rb
@@ -0,0 +1,32 @@
+# @param {String} s
+# @param {Integer} k
+# @return {Integer}
+def character_replacement(s, k)
+  l = 0
+  char_hash = Hash.new(0)
+  longest_substring = 0
+
+  (0...s.length).each do |r|
+    char_hash[s[r]] += 1
+    most_common_char = char_hash.sort_by { |k, v| -v }.first.first
+    other_char_count = char_hash.reduce(0) do |sum, (char, count)|
+      sum + (char == most_common_char ? 0 : count)
+    end
+
+    if other_char_count > k
+      char_hash[s[l]] -= 1
+      l += 1
+      most_common_char = char_hash.sort_by { |k, v| -v }.first.first
+      other_char_count = char_hash.reduce(0) do |sum, (char, count)|
+        sum + (char == most_common_char ? 0 : count)
+      end
+    end
+
+    longest_substring = [longest_substring, char_hash.values.sum].max
+  end
+
+  longest_substring
+end
+
+p character_replacement("ABAB", 2) # 4 "AAAA" or "BBBB"
+p character_replacement("AABABBA", 1) # 4 "AAAABBA", "AABBBBA", etc
