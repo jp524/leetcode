4c3eb0e8bf6c5e61ebf176c2d8337a9a5979cbf6
JP <85654561+jp524@users.noreply.github.com>
2024-05-14 15:50:05 -0500

    19. Remove Nth Node From End of List: faster without 3rd pointer

--- 0019_remove_nth_node_from_end.rb
+++ 0019_remove_nth_node_from_end.rb
@@ -1,5 +1,6 @@
 # 19. Remove Nth Node From End of List
 # Linked List
+# Two Pointers
 
 # Definition for singly-linked list.
 # class ListNode
@@ -14,21 +15,19 @@
 # @return {ListNode}
 def remove_nth_from_end(head, n)
   # Use dummy node in case the node to be removed is the first from the list
-  dummy = ListNode.new
-  dummy.next = head
-  node_to_delete = dummy
-  end_of_list = dummy
+  dummy = ListNode.new(0, head)
+
+  before_node_to_delete = dummy
+  end_of_list = head
   n.times do
     end_of_list = end_of_list.next
   end
 
-  prev = nil
   while end_of_list
-    prev = node_to_delete
-    node_to_delete = node_to_delete.next
+    before_node_to_delete = before_node_to_delete.next
     end_of_list = end_of_list.next
   end
-  prev.next = prev.next.next
-  
+  before_node_to_delete.next = before_node_to_delete.next.next
+
   dummy.next
 end
