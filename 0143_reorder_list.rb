355d4db01fcd6aa60d93c26a534dfd1f452982ec
JP <85654561+jp524@users.noreply.github.com>
2024-05-14 14:30:00 -0500

    143. Reorder List: using slow and fast pointers

--- 0143_reorder_list.rb
+++ 0143_reorder_list.rb
@@ -14,70 +14,36 @@
 def reorder_list(head)
   return head if head.next.nil?
 
-  l = list_length(head) 
-  list1, list2 = split_list(head, l)
-  list2 = reverse_list(list2)
-  merge_two_lists(list1, list2) 
-end
-
-def list_length(head)
-  l = 0
-  curr = head
-  while curr
-    curr = curr.next
-    l += 1
-  end
-  l
-end
-
-def split_list(head, l)
-  i = 0
-  limit = l / 2
-  prev = nil
-  curr = head
-  while i < limit
-    prev = curr
-    curr = curr.next
-    i += 1
+  # Find middle of list
+  slow = head
+  fast = head.next
+  while fast && fast.next
+    slow = slow.next
+    fast = fast.next.next
   end
 
-  prev.next = nil # Modify last node of original linked list
-  [head, curr]
-end
+  list2 = slow.next
+  slow.next = nil # End head (for list1)
 
-def reverse_list(head)
+  # Reverse list2
   prev = nil
-  curr = head
-
+  curr = list2
   while curr
     nxt = curr.next
     curr.next = prev
     prev = curr
     curr = nxt
   end
+  list2 = prev
 
-  prev
-end
-
-def merge_two_lists(list1, list2)
-  dummy = ListNode.new
-  head = dummy
-
-  while list1 && list2
-    dummy.next = list1
-    list1 = list1.next
-    dummy = dummy.next
-
-    dummy.next = list2
-    list2 = list2.next
-    dummy = dummy.next
+  # Merge two lists
+  list1 = head
+  while list2
+    temp1 = list1.next
+    temp2 = list2.next
+    list1.next = list2
+    list2.next = temp1
+    list1 = temp1
+    list2 = temp2
   end
-
-  if list1
-    dummy.next = list1
-  elsif list2
-    dummy.next = list2
-  end
-
-  head.next
 end
