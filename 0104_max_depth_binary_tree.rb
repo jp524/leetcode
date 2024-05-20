17650c9ba9af85e30964317a91b270c997866f81
JP <85654561+jp524@users.noreply.github.com>
2024-05-20 10:45:33 -0500

    104. Maximum Depth of Binary Tree: recursive depth-first search

---
+++ 0104_max_depth_binary_tree.rb
@@ -0,0 +1,22 @@
+# 104. Maximum Depth of Binary Tree
+# Trees
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+  return 0 if root.nil?
+
+  left_depth = max_depth(root.left)
+  right_depth = max_depth(root.right)
+
+  1 + [left_depth, right_depth].max
+end
