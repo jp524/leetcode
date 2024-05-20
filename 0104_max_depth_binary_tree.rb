7b6063e543359243d1848e9e85695112fde5c9ab
JP <85654561+jp524@users.noreply.github.com>
2024-05-20 10:55:50 -0500

    104. Maximum Depth of Binary Tree: iterative breadth-first search

--- 0104_max_depth_binary_tree.rb
+++ 0104_max_depth_binary_tree.rb
@@ -15,8 +15,17 @@
 def max_depth(root)
   return 0 if root.nil?
 
-  left_depth = max_depth(root.left)
-  right_depth = max_depth(root.right)
+  current_level = 0
+  queue = [root]
 
-  1 + [left_depth, right_depth].max
+  until queue.empty?
+    queue.length.times do 
+      current_node = queue.shift
+      queue << current_node.left if current_node.left
+      queue << current_node.right if current_node.right
+    end
+    current_level += 1
+  end
+
+  current_level
 end
